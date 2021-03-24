3faa3c678be579215516403be4cfe7c6902efd9d
saperez17 <saperez@unicauca.edu.co>
2021-03-23 21:15:58 -0500

    remove heroku db config

--- final_project/settings.py
+++ final_project/settings.py
@@ -146,6 +146,6 @@ MEDIA_URL = '/media/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #Heroku: Update database configuration from $DATABASE_URL
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
