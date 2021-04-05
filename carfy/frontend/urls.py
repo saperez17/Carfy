b9b49a147f362d8963fddcf16b9c5a3e460be38f
saperez17 <saperez@unicauca.edu.co>
2021-04-05 09:22:52 -0500

    frontend doing routing

--- carfy/frontend/urls.py
+++ carfy/frontend/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path("login", UserAccountView.as_view(template_name="frontend/UserAccess/login.html"), name="login"),
     path("signin", UserAccountView.as_view(template_name="frontend/UserAccess/signin.html"), name="signin"),
     path('landing_page', index_view),
-    url(r'^.{0}$', index_view)  # regex matches, then lets routing be handled by the frontend. Still needs a / at end. edited from r'^.*/$'
+    url(r'^.*/$', index_view)  # regex matches, then lets routing be handled by the frontend. Still needs a / at end.
+    #  edited from r'^.*/$' new r'^.{0}/$'
 ]
