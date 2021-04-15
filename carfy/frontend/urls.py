0993ce8b7e5e10654f703b7ff0129286a384f442
saperez17 <saperez@unicauca.edu.co>
2021-04-15 17:56:20 -0500

    url conflict fixed

--- carfy/frontend/urls.py
+++ carfy/frontend/urls.py
@@ -9,13 +9,9 @@ urlpatterns = [
     # path("", UserAccountView.as_view()),
     path("login", UserAccountView.as_view(template_name="frontend/UserAccess/login.html"), name="login"),
     path("signin", UserAccountView.as_view(template_name="frontend/UserAccess/signin.html"), name="signin"),
-<<<<<<< HEAD
     path('logout', logout_view, name='logout'),
     path('landing-page', index_view),
     path('check_auth', check_user_auth),
-=======
-    path('landing-page', index_view),
->>>>>>> 2fdd46c07625486ac63b2c95ad13226ded703a46
     url(r'^.*/$', index_view)  # regex matches, then lets routing be handled by the frontend. Still needs a / at end.
     #  edited from r'^.*/$' new r'^.{0}/$'
 ]
